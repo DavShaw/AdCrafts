a99d40625fff9c21f621d7d9ad76739d66c10b2f
David Torres <juandis0246@gmail.com>
2024-10-07 21:06:00 -0500

    a

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log("AdCrafts is running on NestJS");
+  console.log("Listening on port 3000");
+  console.log("Let's jump in and start building some cool stuff!");
 }
 bootstrap();
